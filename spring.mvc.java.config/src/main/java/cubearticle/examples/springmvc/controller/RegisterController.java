cbbceb9729401872fd282e0aa42c1d97e8c890b8
Полина <@Polinoid>
2017-10-01 19:23:45 +0300

    Now it works with data base

--- spring.mvc.java.config/src/main/java/cubearticle/examples/springmvc/controller/RegisterController.java
+++ spring.mvc.java.config/src/main/java/cubearticle/examples/springmvc/controller/RegisterController.java
@@ -1,5 +1,11 @@
 package cubearticle.examples.springmvc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +17,23 @@ public class RegisterController {
 
 	@RequestMapping(value = "register", method = RequestMethod.POST)
 	public String logIn(@RequestParam String email, @RequestParam String name, @RequestParam String password1,
-			Model model) {
-
-		
-		// insert it into db!
-		model.addAttribute("name", name);
-		return "hello";
+			Model model) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		// Had some problem with timezones.. used serverTimezone=Europe/Moscow
+		// idk exactly wtf0))
+		Connection conn = DriverManager
+				.getConnection("jdbc:mysql://localhost/test?user=root&password=1234&serverTimezone=Europe/Moscow");
+		Statement stmt = conn.createStatement();
+		ResultSet res = stmt.executeQuery("SELECT * FROM users WHERE login = \"" + name + "\" OR email = \"" + email + "\";");
+		if (res.next() == false) {
+			stmt.executeUpdate("INSERT INTO users (login, email, password) VALUES (\"" + name + "\", \"" + email
+					+ "\", \"" + password1 + "\");");
+			model.addAttribute("name", name);
+			System.out.println("It`s ok, i created your account");
+			return "hello";
+		}
+		System.out.println("Seems like your user name or your email is used already");
+		return "index";
 	}
 
 }
